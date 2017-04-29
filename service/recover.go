b9ff807adfb133590ce24dccabac4cb21c207a51
Rafael Martins <rafael84@gmail.com>
2017-04-29 11:21:20 -0300

    add more test cases for the recover function

--- service/recover.go
+++ service/recover.go
@@ -1,7 +1,24 @@
 package service
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	saved = map[string]string{
+		"f1": "http://valid.com",
+		"f2": "http://valid.com/2",
+		"f3": "http://valid.com/3",
+	}
+)
 
 func Recover(alias string) (string, error) {
-	return "", errors.New("invalid alias")
+	if strings.TrimSpace(alias) == "" {
+		return "", errors.New("invalid alias")
+	}
+	if url, found := saved[alias]; found {
+		return url, nil
+	}
+	return "", errors.New("alias not found")
 }
--- service/recover_test.go
+++ service/recover_test.go
@@ -19,6 +19,48 @@ func TestRecover(t *testing.T) {
 			ResURL:   "",
 			ResErr:   "invalid alias",
 		},
+		{
+			Scenario: "Bad 2",
+			ReqAlias: " ",
+			ResURL:   "",
+			ResErr:   "invalid alias",
+		},
+		{
+			Scenario: "Not Found 1",
+			ReqAlias: "a",
+			ResURL:   "",
+			ResErr:   "alias not found",
+		},
+		{
+			Scenario: "Not Found 2",
+			ReqAlias: "b",
+			ResURL:   "",
+			ResErr:   "alias not found",
+		},
+		{
+			Scenario: "Not Found 3",
+			ReqAlias: "c",
+			ResURL:   "",
+			ResErr:   "alias not found",
+		},
+		{
+			Scenario: "Found 1",
+			ReqAlias: "f1",
+			ResURL:   "http://valid.com",
+			ResErr:   "",
+		},
+		{
+			Scenario: "Found 2",
+			ReqAlias: "f2",
+			ResURL:   "http://valid.com/2",
+			ResErr:   "",
+		},
+		{
+			Scenario: "Found 3",
+			ReqAlias: "f3",
+			ResURL:   "http://valid.com/3",
+			ResErr:   "",
+		},
 	} {
 		t.Run(tc.Scenario, func(t *testing.T) {
 			resErr := ""
